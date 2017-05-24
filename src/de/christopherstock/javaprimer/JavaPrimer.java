934e815ea5dd69bb43f0f752b5aa340a8918d119
Christopher Stock <email@christopherstock.de>
2017-05-24 09:55:48 +0200

    Initial commit. Added main class and main method.

---
+++ src/de/christopherstock/javaprimer/JavaPrimer.java
@@ -0,0 +1,21 @@
+
+    package de.christopherstock.javaprimer;
+    
+    /*******************************************************************************************************************
+    *   The main class of the Java Primer Project.
+    *
+    *   @author  Christopher Stock
+    *   @version 1.0
+    *******************************************************************************************************************/
+    class JavaPrimer
+    {
+        /***************************************************************************************************************
+        *   The main method that acts as the application's entry point.
+        *
+        *   @param args The command line arguments being passed to the java application.
+        ***************************************************************************************************************/
+        public static final void main(String[] args)
+        {
+            System.out.println("The going is much more than trying from this point.");
+        }
+    }
