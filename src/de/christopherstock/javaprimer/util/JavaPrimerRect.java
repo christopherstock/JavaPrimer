058e978d106cd5d05e2ba8f4d74ab1f7efac0555
Christopher Stock <email@christopherstock.de>
2017-05-26 12:45:02 +0200

    Added rectangle class. Deleted key system.

---
+++ src/de/christopherstock/javaprimer/util/JavaPrimerRect.java
@@ -0,0 +1,53 @@
+
+    package de.christopherstock.javaprimer.util;
+
+    import  java.awt.*;
+
+    /*******************************************************************************************************************
+    *   Represents a rectangle in 2D space.
+    *
+    *   @author  Christopher Stock
+    *   @version 1.0
+    *******************************************************************************************************************/
+    public class JavaPrimerRect
+    {
+        /** The position x of the left edge. */
+        private                 int         x                           = 0;
+        /** The position y of the top edge. */
+        private                 int         y                           = 0;
+        /** The width of the rectangle. */
+        private                 int         width                       = 0;
+        /** The height of the rectangle. */
+        private                 int         height                      = 0;
+        /** The fill color. */
+        private                 Color       color                       = null;
+
+        /***************************************************************************************************************
+        *   Creates a new rectangle in 2D space.
+        *
+        *   @param x      The position x of the left edge.
+        *   @param y      The position y of the top edge.
+        *   @param width  The width of the rectangle.
+        *   @param height The height of the rectangle.
+        *   @param color  The fill color.
+        ***************************************************************************************************************/
+        public JavaPrimerRect(int x, int y, int width, int height, Color color)
+        {
+            this.x      = x;
+            this.y      = y;
+            this.width  = width;
+            this.height = height;
+            this.color  = color;
+        }
+
+        /***************************************************************************************************************
+        *   Draws this rectangle onto the specified drawing context.
+        *
+        *   @param g The drawing context.
+        ***************************************************************************************************************/
+        public void draw(Graphics g)
+        {
+            g.setColor(this.color);
+            g.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
